a37d4f16311c0f83b4e0e2c13bb7611d7c23f5a5
iremonargan <iremonargann@gmail.com>
2021-10-01 14:27:10 +0300

    fixed

--- codeeditor/assets/js/index.js
+++ codeeditor/assets/js/index.js
@@ -1,7 +1,42 @@
+var output = document.getElementById("editor-output");
+var htmlCode = document.getElementById("htmlCode");
+var cssCode = document.getElementById("cssCode");
+var jsCode = document.getElementById("jsCode");
+var editorWindow = document.getElementById("editor-window");
 
 var runCode = document.querySelector("#run");
 runCode.addEventListener('click', () => {
+  editorRun();
+});
 
+var animateCode = document.querySelector("#minimize");
+animateCode.addEventListener('click', () => {
+  animationClsss();
+});
+
+var save = document.getElementById("save");
+function openModal() {
+  var modal = document.getElementById("editor-modal");
+  modal.style.display = 'block';
+}
+
+save.addEventListener("click", function () {
+  openModal();
+});
+
+var cancel = document.getElementById("cancel");
+cancel.addEventListener('click', () => {
+  var modal = document.getElementById("editor-modal");
+  modal.style.display = 'none';
+});
+
+var saveAll = document.getElementById("saveAll");
+saveAll.addEventListener('click', () => {
+  saveAllClass();
+  window.location.reload(true);
+}, false);
+
+function editorRun() {
   var htmlCode = document.getElementById("htmlCode").value;
   var cssCode = "<\style>" + document.getElementById("cssCode").value + "</style>";
   var jsCode = "<script>" + document.getElementById("jsCode").value + "<\/script>";
@@ -9,59 +44,83 @@ runCode.addEventListener('click', () => {
   frame.open();
   frame.write(cssCode + htmlCode + jsCode);
   frame.close();
-});
-var animateCode = document.querySelector("#minimize");
-animateCode.addEventListener('click', () => {
-  var elementAnim = document.getElementById("editor-output");
-  var animSmallHtml = document.getElementById("htmlCode");
-  var animSmallCss = document.getElementById("cssCode");
-  var animSmallJs = document.getElementById("jsCode");
+}
+
+function animationClsss() {
+  var elementAnim = output;
+  var animSmallHtml = htmlCode;
+  var animSmallCss = cssCode;
+  var animSmallJs = jsCode;
   elementAnim.classList.add("animation-big");
   animSmallHtml.classList.add("animation-small");
   animSmallCss.classList.add("animation-small");
   animSmallJs.classList.add("animation-small");
-});
-var save = document.getElementById("save");
+}
 
-save.addEventListener("click", function () {
-  var modal = document.getElementById("editor-modal");
-  modal.style.display = 'block';
-});
-var cancel = document.getElementById("cancel");
-cancel.addEventListener("click", function () {
-  var modal = document.getElementById("editor-modal");
-  modal.style.display = 'none';
-});
-
-var saveAll = document.getElementById("saveAll");
-
-saveAll.addEventListener("click", function () {
-  var htmlCode = document.getElementById("htmlCode").value;
-  var cssCode = document.getElementById("cssCode").value;
-  var jsCode = document.getElementById("jsCode").value;
-  var codeName = document.getElementById("codename").value;
-  var testObject = { 'htmlCode': htmlCode, 'cssCode': cssCode, 'jsCode': jsCode };
-  localStorage.setItem(codeName, JSON.stringify(testObject));
+function saveAllClass() {
+  var htmlCodes = htmlCode.value;
+  var cssCodes = cssCode.value;
+  var jsCodes = jsCode.value;
+  var codeNames = document.getElementById("codename").value;
+  var keyObject = { 'htmlCode': htmlCodes, 'cssCode': cssCodes, 'jsCode': jsCodes };
+  localStorage.setItem(codeNames, JSON.stringify(keyObject));
   var modal = document.getElementById("editor-modal");
   modal.style.display = 'none';
-}, false);
+}
 
 var keys = Object.keys(localStorage);
 for (var key of keys) {
-  document.getElementById("history-opt").append(new Option(key));
+  document.getElementById("history").append(new Option(key));
 }
-var elements = document.querySelectorAll("#history-opt option");
 
-elements.forEach(function (element) {
-  element.addEventListener('click', function () {
-    key = element.value;
-    var test = JSON.parse(localStorage.getItem(key));
-    console.log(test);
-    htmlCode.value = test.htmlCode;
-    cssCode.value = test.cssCode;
-    jsCode.value = test.jsCode;
-  });
+var elements = document.getElementById("history");
+elements.addEventListener('change', (event) => {
+  key = event.target.value;
+  var keyEvent = JSON.parse(localStorage.getItem(key));
+  htmlCode.value = keyEvent.htmlCode;
+  cssCode.value = keyEvent.cssCode;
+  jsCode.value = keyEvent.jsCode;
+  editorRun();
 });
 
+document.addEventListener('keydown', (e) => {
+  if ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.keyCode == 83) {
+    e.preventDefault();
+    openModal();
+  }
+}, false);
 
+document.addEventListener('keydown', () => {
+  var htmlCodes = htmlCode.value;
+  var cssCodes = cssCode.value;
+  var jsCodes = jsCode.value;
+  var codeName = 'pageRefresing'
+  var keyObject = { 'htmlCode': htmlCodes, 'cssCode': cssCodes, 'jsCode': jsCodes };
+  localStorage.setItem(codeName, JSON.stringify(keyObject));
+});
+if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+  var keyEvent = JSON.parse(localStorage.getItem('pageRefresing'));
+  htmlCode.value = keyEvent.htmlCode;
+  cssCode.value = keyEvent.cssCode;
+  jsCode.value = keyEvent.jsCode;
+  editorRun();
+
+}
+var optionpageRefresing = document.querySelectorAll("option");
+optionpageRefresing.forEach((element) => {
+  if (element.value == "pageRefresing") {
+    element.style.display = 'none';
+  }
+});
+
+const elem = document.querySelectorAll('.editor-code textarea');
+elem.forEach((element) => {
+  document.addEventListener('click', () => {
+    if (element === document.activeElement) {
+      element.style.setProperty("height", "400px", "important");
+    } else {
+      element.style.height = '200px';
+    }
+  });
+});
 
